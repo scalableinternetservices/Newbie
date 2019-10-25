cca0fe1d9d9648711ef264aa85119c51690043bc
Shashwat Goel <shashwat@Shashwats-MacBook-Pro.local>
2019-10-24 18:58:10 -0700

    Added Basic Functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	resources :output
+	resources :main
+	root 'application#team'
 end
