57d398144a70c778e656b023d8dcbed6f990d604
wahjay <hotan38@berkeley.edu>
2019-11-01 13:54:21 -0700

    database migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_11_01_205225) do
+
+  create_table "searches", force: :cascade do |t|
+    t.string "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
