ef2ed91755ceca5187d0d6a4d692f2442055f4b8
wahjay <hotan38@berkeley.edu>
2019-11-01 03:43:19 -0700

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get '/articles', to: 'articles#index', as: 'article'
 
+  get '/articles/new', to: 'articles#new'
+  get '/articles/:id', to: 'articles#show'
+
+
   resources :articles
 	#resources :output
 	#resources :main
