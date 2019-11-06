49a60111928e9e43d64b3803ef26b5d18d8c754e
wahjay <hotan38@berkeley.edu>
2019-11-05 16:40:16 -0800

    link db table

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,4 +1,5 @@
 class Search < ApplicationRecord
+  belongs_to :user
   validates :text, presence: true
   validates :score, presence:false, numericality: { :less_than_or_equal_to => 100, :greater_than_or_equal_to => 0}
 end
