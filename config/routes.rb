9dbc910323ddc28ae15ce1e4533bb52d6cfb233d
Weilan Yu <isweilanyu@gmail.com>
2019-11-05 21:09:47 -0800

    Create user controller for profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
+  
   root 'searches#index'
   devise_for :users
+  resources :users
+  get 'users/show'
+  
   get 'welcome/index'
   get '/searches', to: 'searches#index', as: 'search'
   resources :searches
+
 
   get '/searches/new', to: 'searches#new'
 
