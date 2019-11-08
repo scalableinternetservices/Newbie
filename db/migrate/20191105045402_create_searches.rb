5a8476b44b422ea2e898cf3162b84717b4240ca0
wahjay <hotan38@berkeley.edu>
2019-11-08 15:22:19 -0800

    allow user id to be null for public search

--- db/migrate/20191105045402_create_searches.rb
+++ db/migrate/20191105045402_create_searches.rb
@@ -2,7 +2,7 @@ class CreateSearches < ActiveRecord::Migration[6.0]
   def change
     create_table :searches do |t|
       t.string :text
-      t.references :user, null:false, foreign_key:true
+      t.references :user, null:true, foreign_key:true
       t.timestamps
     end
   end
