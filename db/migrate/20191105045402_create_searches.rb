4d9ed47ce95d23cf91282fc11cc707793ff34e5a
wahjay <hotan38@berkeley.edu>
2019-11-08 15:36:57 -0800

    testing..

--- db/migrate/20191105045402_create_searches.rb
+++ db/migrate/20191105045402_create_searches.rb
@@ -2,7 +2,7 @@ class CreateSearches < ActiveRecord::Migration[6.0]
   def change
     create_table :searches do |t|
       t.string :text
-      t.references :user, null:true, foreign_key:true
+      t.references :user, :user_id :null=>true, foreign_key:true
       t.timestamps
     end
   end
