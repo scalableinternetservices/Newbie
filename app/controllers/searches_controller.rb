b338c34e40632195e071054dc067083928bca5a6
Rishabh Jain <rishabh.jain1198@gmail.com>
2019-11-14 16:20:51 -0800

    Added search functionality

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -32,14 +32,14 @@ class SearchesController < ApplicationController
 
     #check if user signs in or not
     @search = nil
-    @user = User.find(1) if User.all != nil
+    @user = User.find(1) if User.count > 0
     if current_user.nil?
       @search = Search.create(search_params)
-      #all the public search without signing in will be marked with user_id equlas to 1
+      #all the public search without signing in will be marked with user_id equals to 1
       if @user.nil?
-        @user = User.new(user_name:"admin",emial:"admin@newbie.com",encrypted_password:"012340")
+        @user = User.new(user_name:"admin",email:"admin@newbie.com",encrypted_password:"012340", password:"012340")
         @user.id = 1
-        @user.save
+        @user.save!
       end
       @search = @user.searches.create(search_params)
       #@search.user_id = 0
@@ -49,9 +49,12 @@ class SearchesController < ApplicationController
       p @search
     end
 
+    # GET SCORE FOR THIS SEARCH
+    results = get_results(@search.text)
+    @search.score = results["score"]
+
     #for testing, default score value 66
     @search.score = 100 if @search.score.nil?
-
 
     respond_to do |format|
       if @search.save
@@ -100,6 +103,26 @@ class SearchesController < ApplicationController
         flash[:error] = "You must be logged in to access this section."
         redirect_to new_login_url
       end
+    end
+
+    def get_results(search_text)
+      results = {}
+      matching_articles = Article.search(search_text).with_pg_search_rank
+      total_score = 0
+      
+      counter = 0
+      matching_ids = []
+      for article in matching_articles do
+        total_score += article.pg_search_rank
+        counter += 1
+        matching_ids.append(article.id)
+      end
+
+      saver = 0
+      saver = total_score / counter if counter != 0
+      results["score"] = saver * 100
+      results["matching_ids"] = matching_ids
+      results
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
