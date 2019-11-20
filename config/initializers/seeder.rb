a28aec78545d3448902fb4959553917809bb29e6
Rishabh Jain <rishabh.jain1198@gmail.com>
2019-11-20 14:41:02 -0800

    Add leader only flag

--- config/initializers/seeder.rb
+++
@@ -1,7 +0,0 @@
-if not ActiveRecord::Base.connection.table_exists? 'articles'
-  `bundle exec rails db:migrate`
-end
-if Article.count == 0
-  puts("Starting seeding database")
-  seeding_process = `nohup bundle exec rails db:seed >/dev/null 2>&1 &`
-end
