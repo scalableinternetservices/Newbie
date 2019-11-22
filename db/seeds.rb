621e4d5baaca9299e70a24606310e41583f0d78a
Weilan Yu <isweilanyu@gmail.com>
2019-11-22 13:05:08 -0800

    ...

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
 require 'jsonl'
 
-# dirname = File.join(File.dirname(__FILE__), 'seed')
-# parts = Dir.entries(dirname).select {|f| !File.directory? f}
-# json = ''
-# parts.each do |partname|
-#   json = File.read(File.join(dirname, partname))
-#   records = JSONL.parse(json)
-#   to_save = []
-#   records.each do |record|
-#     x = {}
-#     x["title"] = record["title"]
-#     x["body"] = record["text"]
-#     x["date_published"] = record["date"]
-#     x["summary"] = record["summary"]
-#     x["url"] = record["url"]
-#     to_save.append(x)
-#   end
-#   Article.create(to_save)
-# end
+dirname = File.join(File.dirname(__FILE__), 'seed')
+parts = Dir.entries(dirname).select {|f| !File.directory? f}
+json = ''
+parts.each do |partname|
+  json = File.read(File.join(dirname, partname))
+  records = JSONL.parse(json)
+  to_save = []
+  records.each do |record|
+    x = {}
+    x["title"] = record["title"]
+    x["body"] = record["text"]
+    x["date_published"] = record["date"]
+    x["summary"] = record["summary"]
+    x["url"] = record["url"]
+    to_save.append(x)
+  end
+  Article.create(to_save)
+end
 
 # For user relationship test only
 # Users
